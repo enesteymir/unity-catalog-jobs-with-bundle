5267acafe34b577aeb4ea389c1930232cb861864
Enes Teymir <enesteymir@gmail.com>
2025-06-05 23:20:15 +0300

    Delete job_sources/jobs directory

--- job_sources/jobs/create_external_table_example.sql
+++
@@ -1,3 +0,0 @@
-DROP TABLE IF EXISTS customer_360_catalog_test.archive.message_psd;
-CREATE EXTERNAL TABLE customer_360_catalog_test.archive.message_psd
-LOCATION "s3://customer-360-data-platform-test/archive/message_psd";
